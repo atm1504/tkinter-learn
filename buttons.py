35b98bb9ccf6ccbb14a06a8f119ec8c72d034a26
Amartya Mondal <atm1504.in@gmail.com>
2020-12-23 08:55:26 +0530

    Button added

---
+++ buttons.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+root = Tk()
+
+# -> show this number on click
+count=0
+def myClick():
+    global count
+    count += 1;
+    myLabel = Label(root, text="Current count is: " + str(count))
+    myLabel.pack()
+
+
+myButton = Button(root, text="First Button", padx=10, pady=10, command=myClick, fg="#000000", bg="black")
+myButton.pack()
+
+
+root.mainloop()
