91799ee47d05ca86729eb51fd3705cbd9354b4d1
Amartya Mondal <atm1504.in@gmail.com>
2020-12-24 22:28:38 +0530

    table db created

--- crm.py
+++ crm.py
@@ -11,9 +11,20 @@ root.geometry("400x400")
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="11312113"
+    passwd="11312113",
+    database="codemy"
 )
 
-print(mydb)
+# Create a cursor and initialize it
+my_cursor = mydb.cursor()
+
+# Database created
+# my_cursor.execute("CREATE DATABASE codemy");
+
+# Table created
+# my_cursor.execute("CREATE TABLE customers( first_name VARCHAR(255), last_name VARCHAR(255) , zipcode int(10), price_paid DECIMAL(10, 2), user_id INT AUTO_INCREMENT PRIMARY KEY) ")
+
+my_cursor.execute("SELECT * FROM customers")
+print(my_cursor.description)
 
 mainloop();
