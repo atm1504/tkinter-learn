85190881c030204b29e2f2afcc5e03eb46f70fdd
Amartya Mondal <atm1504.in@gmail.com>
2020-12-24 21:26:14 +0530

    Matplotlib

---
+++ weather.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+
+root = Tk()
+root.title("Learning Tkinter")
+root.iconbitmap("./images/quality.ico")
+root.geometry("400x400")
+
+
+mainloop();
