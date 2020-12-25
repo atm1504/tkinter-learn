318fbb82f39f9ba7dac77d680f6ec3c54a5c28fa
Amartya Mondal <atm1504.in@gmail.com>
2020-12-25 17:17:02 +0530

    Forget

--- forget.py
+++ forget.py
@@ -7,26 +7,31 @@ root.title("Learning Tkinter")
 root.iconbitmap("./images/quality.ico")
 root.geometry("400x400")
 
+myLabel = Label(root)
+
 def myDelete():
-    myLabel.pack_forget()
+    myLabel.grid_forget()
     myButton['state'] = NORMAL
 
 def myClick():
     global myLabel
+    myLabel = Label(root)
+    myLabel.destroy()
+
     hello = "Hello " + e.get()
     myLabel = Label(root, text=hello)
     e.delete(0, 'end')
-    myLabel.pack(pady=10)
-    myButton['state'] = DISABLED
+    myLabel.grid(row=3, column=0, pady=10)
+    # myButton['state'] = DISABLED
 
 e = Entry(root, width=50, font=('Helvetica', 30))
-e.pack(padx=10, pady=10)
+e.grid(row=0, column=0, padx=10, pady=10)
 
 myButton = Button(root, text="Enter your name!", command=myClick)
-myButton.pack(pady=10)
+myButton.grid(pady=10,row=1,column=0)
 
 deleteButton = Button(root, text="Delete Text", command=myDelete)
-deleteButton.pack(pady=10)
+deleteButton.grid(pady=10, row=2, column=0)
 
 
 mainloop();
