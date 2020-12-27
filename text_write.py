0f0e1a599ee4caec7ddab630b89f1921a764fe68
Amartya Mondal <atm1504.in@gmail.com>
2020-12-27 22:24:54 +0530

    Text file save

---
+++ text_write.py
@@ -0,0 +1,39 @@
+from tkinter import *
+from PIL import ImageTk, Image
+from tkinter import filedialog
+
+
+root = Tk()
+root.title("Learning Tkinter")
+root.iconbitmap("./images/quality.ico")
+root.geometry("400x400")
+
+def open_txt():
+    text_file = filedialog.askopenfilename(initialdir="", title="Open text File", filetypes=(("Text Files", "*.txt"),))
+
+    text_file = open(text_file, "r")
+    stuff = text_file.read()
+
+    my_text.insert(END,stuff)
+    text_file.close()
+
+# def save_txt():
+#     text_file = filedialog.askopenfilename(initialdir="", title="Open Text File", filetypes=(("Text Files", "*.txt"),))
+# 	text_file = open(text_file, 'w')
+# 	text_file.write(my_text.get(1.0, END))
+
+def save_txt():
+    text_file = filedialog.askopenfilename(initialdir="", title="OPen text file", filetypes=(("Text Files", "*.txt"),))
+    text_file = open(text_file, 'a')
+    text_file.write(my_text.get(1.0, END))
+
+my_text = Text(root, width=40, height=10, font=("Helvetica", 16))
+my_text.pack(pady=20)
+
+open_button = Button(root, text="Open text file", command=open_txt)
+open_button.pack(pady=20)
+
+save_button = Button(root, text="Save File", command=save_txt)
+save_button.pack(pady=20)
+
+root.mainloop();
