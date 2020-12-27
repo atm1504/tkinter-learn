786fef053ae2404b8c347f4c6783a30fc9069488
Amartya Mondal <atm1504.in@gmail.com>
2020-12-27 12:35:05 +0530

    Shuffle game

---
+++ info.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+
+root = Tk()
+root.title("Learning Tkinter")
+root.iconbitmap("./images/quality.ico")
+root.geometry("800x800+-1900+100")
+
+
+root.mainloop();
