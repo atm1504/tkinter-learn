bde8e91329edf82c69b609fb746c9d3732aba3fd
Amartya Mondal <atm1504.in@gmail.com>
2020-12-27 17:28:06 +0530

    Delete songs, next, prrevious

--- player.py
+++ player.py
@@ -56,6 +56,42 @@ def pause(is_paused):
         pygame.mixer.music.pause()
         paused = True
     
+def nextSong():
+    next_one = songBox.curselection()
+    next_one = next_one[0] + 1
+    song = songBox.get(next_one)
+
+    song = f'/Users/amartyamondal/Documents/project/tkinter/audio/{song}.mp3'
+    pygame.mixer.music.load(song)
+    pygame.mixer.music.play(loops=0)
+
+    songBox.selection_clear(0, END)
+    songBox.activate(next_one)
+    songBox.selection_set(next_one, last=None)
+
+def previousSong():
+    next_one = songBox.curselection()
+    next_one = next_one[0] - 1
+    song = songBox.get(next_one)
+
+    song = f'/Users/amartyamondal/Documents/project/tkinter/audio/{song}.mp3'
+    pygame.mixer.music.load(song)
+    pygame.mixer.music.play(loops=0)
+
+    songBox.selection_clear(0, END)
+    songBox.activate(next_one)
+    songBox.selection_set(next_one, last=None)
+
+# Delete A Song
+def delete_song():
+	songBox.delete(ANCHOR)
+	pygame.mixer.music.stop()
+
+# Delete All Songs from Playlist
+def delete_all_songs():
+	songBox.delete(0, END)
+	pygame.mixer.music.stop()
+
 songBox = Listbox(root, bg="black", fg="green", width=60, selectbackground ="gray", selectforeground="black")
 songBox.pack(pady=20)
 
@@ -71,8 +107,8 @@ controlFrame = Frame(root)
 controlFrame.pack()
 
 # Create Player Control Buttons
-back_button = Button(controlFrame, image=back_btn_img, borderwidth=0)
-forward_button = Button(controlFrame, image=forward_btn_img, borderwidth=0)
+back_button = Button(controlFrame, image=back_btn_img, borderwidth=0, command=previousSong)
+forward_button = Button(controlFrame, image=forward_btn_img, borderwidth=0, command=nextSong)
 play_button = Button(controlFrame, image=play_btn_img, borderwidth=0, command=play)
 pause_button = Button(controlFrame, image=pause_btn_img, borderwidth=0, command=lambda:pause(paused))
 stop_button = Button(controlFrame, image=stop_btn_img, borderwidth=0, command=stop)
@@ -93,5 +129,11 @@ addSongMenu = Menu(myMenu)
 myMenu.add_cascade(label="Add songs", menu=addSongMenu)
 addSongMenu.add_command(label="Add One Song to Playlist", command=addSong)
 addSongMenu.add_command(label="Add Many Song to Playlist", command=addManySongs)
+
+# Remove song menu
+remove_song_menu = Menu(myMenu)
+myMenu.add_cascade(label="Remove Songs", menu=remove_song_menu)
+remove_song_menu.add_command(label="Delete A Song From Playlist", command=delete_song)
+remove_song_menu.add_command(label="Delete All Songs From Playlist", command=delete_all_songs)
 
 root.mainloop();
