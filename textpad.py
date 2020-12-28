28e82bba980685965774eab839224a9edfe792cc
Amartya Mondal <atm1504.in@gmail.com>
2020-12-28 20:31:05 +0530

    Save files

--- textpad.py
+++ textpad.py
@@ -8,6 +8,48 @@ root.title("Learning Tkinter")
 root.iconbitmap("./images/quality.ico")
 root.geometry("1200x660")
 
+#-----------------------------------------------------Functions-----------------------------------------------------------------
+#Create new file function
+def new_file():
+    my_text.delete("1.0", END)
+    root.title('New File - TextPad!')
+    status_bar.config(text="New File      ")
+
+
+#Create new file function
+def open_file():
+    # Clear Editor
+    my_text.delete("1.0", END)
+    # Grab File
+    text_file = filedialog.askopenfilename(initialdir="", title="Open text File", filetypes=(("Text Files", "*.txt"), ("HTML Files", "*.html"), ("Python Files", "*.py"), ("All Files", "*.*"),))
+    name = text_file
+    status_bar.config(text=f'{name}           ')
+    root.title(f'{name} - TextPad!')
+
+    # Open the file
+    text_file = open(text_file, 'r')
+    stuff = text_file.read()
+
+    # Add file data tot he text box
+    my_text.insert(END, stuff)
+    # Close  file
+    text_file.close()
+
+def save_as_file():
+    text_file = filedialog.asksaveasfilename(defaultextension=".*", initialdir="", title="Save File", filetypes=(("Text Files", "*.txt"), ("HTML Files", "*.html"), ("Python Files", "*.py"), ("All Files", "*.*")))
+    if text_file:
+        name = text_file
+        root.title(f'{name} - TextPad!')
+        status_bar.config(text=f'Saved: {name}           ')
+
+        # Save  the file
+        text_file = open(text_file, 'w')
+        text_file.write(my_text.get(1.0, END))
+        # Close the file
+        text_file.close()
+
+
+#--------------------------------------------------------------------------------------------------------------
 # Create Main Frame
 my_frame = Frame(root)
 my_frame.pack(pady=5)
@@ -16,11 +58,9 @@ my_frame.pack(pady=5)
 text_scroll = Scrollbar(my_frame)
 text_scroll.pack(side=RIGHT, fill=Y)
 
-
 # Text box
 my_text = Text(my_frame, width=97, height=25, font=("Helvetica", 16), selectbackground="yellow", selectforeground="black", undo=True, yscrollcommand=text_scroll.set, wrap="none")
 my_text.pack(pady=5)
-
 
 # Configure our scroll bar
 text_scroll.config(command=my_text.yview)
@@ -31,9 +71,10 @@ root.config(menu=my_menu)
 # File menu
 file_menu = Menu(my_menu, tearoff=False)
 my_menu.add_cascade(label="File", menu=file_menu)
-file_menu.add_command(label="New")
-file_menu.add_command(label="Open")
-file_menu.add_command(label="Save")
+file_menu.add_command(label="New", command=new_file)
+file_menu.add_command(label="Open", command=open_file)
+file_menu.add_command(label="Save", command=save_as_file)
+file_menu.add_command(label="Save As")
 file_menu.add_separator()
 file_menu.add_command(label="Exit", command= root.quit)
 
