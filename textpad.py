f34ea7866e44512c467b4d646e10a7c5adbdf4d2
Amartya Mondal <atm1504.in@gmail.com>
2020-12-29 14:45:26 +0530

    Textpad themes added

--- textpad.py
+++ textpad.py
@@ -175,6 +175,50 @@ def select_all(e):
 def clear_all():
     my_text.delete(1.0, END)
 
+# Turn on Night Mode
+def night_on():
+    main_color = "#000000"
+    second_color = "#373737"
+    text_color = "green"
+
+    root.config(bg=main_color)
+    status_bar.config(bg=main_color, fg=text_color)
+    my_text.config(bg=second_color)
+    toolbar_frame.config(bg=main_color)
+
+    bold_button.config(bg=second_color)
+    italics_button.config(bg=second_color)
+    redo_button.config(bg=second_color)
+    undo_button.config(bg=second_color)
+    color_text_button.config(bg=second_color)
+
+    file_menu.config(bg=main_color,fg=text_color)
+    edit_menu.config(bg=main_color,fg=text_color)
+    options_menu.config(bg=main_color,fg=text_color)
+    color_menu.config(bg=main_color,fg=text_color)
+
+
+# Turn off Night Mode
+def night_off():
+    main_color = "SystemButtonFace"
+    second_color = "SystemButtonFace"
+    text_color = "black"
+
+    root.config(bg=main_color)
+    status_bar.config(bg=main_color, fg=text_color)
+    my_text.config(bg=second_color)
+    toolbar_frame.config(bg=main_color)
+
+    bold_button.config(bg=second_color)
+    italics_button.config(bg=second_color)
+    redo_button.config(bg=second_color)
+    undo_button.config(bg=second_color)
+    color_text_button.config(bg=second_color)
+
+    file_menu.config(bg=main_color,fg=text_color)
+    edit_menu.config(bg=main_color,fg=text_color)
+    options_menu.config(bg=main_color,fg=text_color)
+    color_menu.config(bg=main_color,fg=text_color)
 #--------------------------------------------------------------------------------------------------------------
 # Create a toolbar frame
 toolbar_frame = Frame(root)
@@ -235,6 +279,12 @@ my_menu.add_cascade(label="Color", menu=color_menu)
 color_menu.add_command(label="Change Selected", command=text_color)
 color_menu.add_command(label="All Text", command=all_text_color)
 color_menu.add_command(label="Background", command=bg_color)
+
+# Add Options menu
+options_menu = Menu(my_menu)
+my_menu.add_cascade(label="Options", menu=options_menu)
+options_menu.add_command(label="Dark Mode On", command=night_on)
+options_menu.add_command(label="Dark Mode Off", command=night_off)
 
 # Add Status Bar
 status_bar = Label(root, text='Ready         ', anchor=E)
