d69d78f42f640cd582b9d358a20d6f78ed979e32
Amartya Mondal <atm1504.in@gmail.com>
2020-12-29 16:42:06 +0530

    Delete add option added

--- tree.py
+++ tree.py
@@ -5,7 +5,7 @@ from tkinter import ttk
 root = Tk()
 root.title("Learning Tkinter")
 root.iconbitmap("./images/quality.ico")
-root.geometry("500x500")
+root.geometry("800x800")
 
 my_tree = ttk.Treeview(root)
 
@@ -14,9 +14,9 @@ my_tree['columns'] = ("Name", "ID", "Favorite Pizza")
 
 # Formate our columns
 my_tree.column("#0", width=0, stretch=NO)
-my_tree.column("Name", anchor=W, width=120)
+my_tree.column("Name", anchor=W, width=140)
 my_tree.column("ID", anchor=CENTER, width=80)
-my_tree.column("Favorite Pizza", anchor=W, width=120)
+my_tree.column("Favorite Pizza", anchor=W, width=140)
 
 # Create Headings
 my_tree.heading("#0", text="", anchor=W)
@@ -26,30 +26,116 @@ my_tree.heading("Favorite Pizza", text="Favorite Pizza", anchor=W)
 
 # Datas
 data = [
-    ["John1", 1, "Pepperroni"],
-    ["John2", 2, "Pepperroni"],
-    ["John3", 3, "Pepperroni"],
-    ["John4", 4, "Pepperroni"]
+	["John", 1, "Pepperoni"],
+	["Mary", 2, "Cheese"],
+	["Tim", 3, "Mushroom"],
+	["Erin", 4, "Ham"],
+	["Bob", 5, "Onion"],
+	["Steve", 6, "Peppers"],
+	["Tina", 7, "Cheese"],
+	["Mark", 8, "Supreme"],
+	["John", 1, "Pepperoni"],
+	["Mary", 2, "Cheese"],
+	["Tim", 3, "Mushroom"],
+	["Erin", 4, "Ham"],
+	["Bob", 5, "Onion"],
+	["Steve", 6, "Peppers"],
+	["Tina", 7, "Cheese"],
+	["Mark", 8, "Supreme"],
+	["John", 1, "Pepperoni"],
+	["Mary", 2, "Cheese"],
+	["Tim", 3, "Mushroom"],
+	["Erin", 4, "Ham"],
+	["Bob", 5, "Onion"],
+	["Steve", 6, "Peppers"],
+	["Tina", 7, "Cheese"],
+	["Mark", 8, "Supreme"],
+	["Ruth", 9, "Vegan"]
 ]
 
+# data = [
+# 	["John", 1, "Pepperoni"],
+# 	["Mary", 2, "Cheese"],
+# 	["Tim", 3, "Mushroom"],
+# 	["Erin", 4, "Ham"],
+# 	["Bob", 5, "Onion"],
+# 	["Steve", 6, "Peppers"],
+# 	["Tina", 7, "Cheese"],
+# 	["Mark", 8, "Supreme"]
+# ]
+global count
 count = 0
 for record in data:
     my_tree.insert(parent='', index=END, iid=count, text="Parent", values=(record[0], record[1], record[2]))
     count += 1
 
-# Add Data
-# my_tree.insert(parent='', index=END, iid=0, text="Parent", values=("John", 1, "Peperroni"))
-# my_tree.insert(parent='', index=END, iid=1, text="Parent", values=("Tass1", 2, "yuexuiqn1"))
-# my_tree.insert(parent='', index=END, iid=2, text="Parent", values=("Tass2", 3, "yuexuiqn2"))
-# my_tree.insert(parent='', index=END, iid=3, text="Parent", values=("Tass3", 4, "yuexuiqn3"))
-# my_tree.insert(parent='', index=END, iid=4, text="Parent", values=("Tass4", 5, "yuexuiqn4"))
-# my_tree.insert(parent='', index=END, iid=5, text="Parent", values=("Tass5", 6, "yuexuiqn5"))
 my_tree.pack(pady=20)
 
+add_frame = Frame(root)
+add_frame.pack(pady=20)
+
+# Name label
+nl = Label(add_frame, text="Name")
+nl.grid(row=0, column=0)
+
+# ID label
+il = Label(add_frame, text="ID")
+il.grid(row=0, column=1)
+
+# Topping label
+tl = Label(add_frame, text="Topping")
+tl.grid(row=0, column=2)
+
+# Entry boxes
+name_box = Entry(add_frame)
+name_box.grid(row=1, column=0)
+
+id_box = Entry(add_frame)
+id_box.grid(row=1, column=1)
+
+topping_box = Entry(add_frame)
+topping_box.grid(row=1, column=2)
+
 # Add child
-my_tree.insert(parent='', index=END, iid=6, text="Child", values=("Steve", 7, "wetryuio"))
-my_tree.move('6', '0', '0')
+# my_tree.insert(parent='', index=END, iid=6, text="Child", values=("Steve", 7, "wetryuio"))
+# my_tree.move('6', '0', '0')
+
+def add_record():
+    global count
+    my_tree.insert(parent='', index=END, iid=count, text="Child", values=(name_box.get(), id_box.get(), topping_box.get()))
+    count += 1
+    name_box.delete(0, END)
+    id_box.delete(0, END)
+    topping_box.delete(0, END)
+
+def remove_all():
+    for record in my_tree.get_children():
+        my_tree.delete(record)
+
+def remove_one():
+    rec = my_tree.selection()[0]
+    my_tree.delete(rec)
+
+def remove_many():
+    records = my_tree.selection()
+    for rec in records:
+        my_tree.delete(rec)
 
 
+# Add an item Buttons
+add_record = Button(root, text="Add Record", command=add_record)
+add_record.pack(pady=20)
+
+# Remove all elements
+remove_all = Button(root, text="Remove All", command=remove_all)
+remove_all.pack(pady=10)
+
+# Remove many elements
+remove_many = Button(root, text="Remove Many Selected", command=remove_many)
+remove_many.pack(pady=10)
+
+# Remove one elements
+remove_one = Button(root, text="Remove One", command=remove_one)
+remove_one.pack(pady=10)
 
 root.mainloop();
